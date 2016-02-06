5638f5405c7bdb862ff55b448d7cbe1495a6736e
ArturBK <artur.koefender@ilegra.com>
2016-02-06 19:24:04 -0200

    edit e remove

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,13 +1,30 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http) {
+    .controller('FotoController', function($scope, $http, $routeParams) {
 
         $scope.foto = {};
         $scope.mensagem = '';
 
+        if($routeParams.fotoId) {
+            $http.get('/v1/fotos/' + $routeParams.fotoId)
+            .success(function(foto) {
+                $scope.foto = foto;
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível obter a foto'
+            });
+        }
+
         $scope.submeter = function() {
 
             if ($scope.formulario.$valid) {
-
+                if($scope.foto._id)  { 
+                    $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                    .success(function(){
+                        $scope.mensagem = 'Foto cadastrada com sucesso';
+                    });
+                }
+                else{
                 $http.post('/v1/fotos', $scope.foto)
                 .success(function() {
                     $scope.foto = {};
@@ -17,6 +34,7 @@ angular.module('alurapic')
                     console.log(erro);
                     $scope.mensagem = 'Não foi possível cadastrar a foto';
                 })
+            }
             }
         };
 
